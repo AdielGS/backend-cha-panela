82d8b3a139c894ece48c80516f552e8745b9b210
AdielGS <adielgsalviano@gmail.com>
2026-04-20 10:49:36 -0300

    Ensinando o render a ler pastas adieljulia

--- adieljulia/src/main/java/com/chadepanela/adieljulia/AdieljuliaApplication.java
+++ adieljulia/src/main/java/com/chadepanela/adieljulia/AdieljuliaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdielJuliaApplication {
+public class AdieljuliaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdielJuliaApplication.class, args);
+		SpringApplication.run(AdieljuliaApplication.class, args); // <-- J minúsculo aqui também!
 	}
 
 }
